b89164427db2c600b14d95420eadbb0e785a680f
Juan <maurino208@gmail.com>
2020-12-01 18:45:37 -0300

    working with the routes

--- back/app.js
+++ back/app.js
@@ -1,6 +1,18 @@
-const http = require("http");
-const routes = require("./routes/routes");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+});
+
+app.listen(3000);
