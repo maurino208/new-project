e3d34dee82e4a4e78ca4059912d31f8e63b8134b
Juan <maurino208@gmail.com>
2020-12-01 18:45:03 -0300

    adding components with their styles

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AddProductsComponent } from './components/shop/add-products/add-products.component';
+import { NotFound404Component } from './components/not-found404/not-found404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopComponent,
+    NavbarComponent,
+    AddProductsComponent,
+    NotFound404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
