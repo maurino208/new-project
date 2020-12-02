da7c29d2289aef63e8cf4c5d42e623fb8afd90f4
Juan <maurino208@gmail.com>
2020-12-02 16:17:24 -0300

    working on admin and shop routes

--- back/routes/shop.js
+++ back/routes/shop.js
@@ -1,9 +1,21 @@
+const path = require('path');
+
 const express = require('express');
+
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.send('<h1>Hello World!</h1>');
-});
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:id', shopController.getProductById);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
