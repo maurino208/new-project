21543785b1a431cae95b79be331f8725300a02c6
Juan <maurino208@gmail.com>
2020-12-02 17:04:57 -0300

    working on shop controller - adding cart

--- back/controllers/shop.js
+++ back/controllers/shop.js
@@ -10,11 +10,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.getProductById = (req, res, next) => {
-  const _id = req.params.id;
-  console.log(_id);
-  res.redirect('/')
-}
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -31,6 +36,12 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.prodId;
+  console.log(prodId);
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
